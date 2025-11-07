3db70751cc205ce21cf8affd5191c0f161459e7f
MinecAnton209 <poterbaanton90@gmail.com>
2025-11-07 17:45:18 +0200

    feat: Implement i18n infrastructure with .resx files

---
+++ Watermarked/Localisation/TranslateExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Resources;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+
+namespace Watermarked.Localisation;
+
+public class TranslateExtension : MarkupExtension
+{
+    private static readonly ResourceManager ResManager = 
+        new("Watermarked.Resources.Strings", typeof(TranslateExtension).Assembly);
+
+    public string Key { get; set; }
+
+    public TranslateExtension(string key)
+    {
+        Key = key;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        try
+        {
+            return ResManager.GetString(Key) ?? $"#{Key}#";
+        }
+        catch
+        {
+            return $"!{Key}!";
+        }
+    }
+}
