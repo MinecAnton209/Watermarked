8d2da460439569169a17473a733965a6608b3403
MinecAnton209 <poterbaanton90@gmail.com>
2025-11-07 17:58:58 +0200

    feat: Display selected image in the preview area

--- Watermarked/ViewModels/MainViewModel.cs
+++ Watermarked/ViewModels/MainViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 
 namespace Watermarked.ViewModels;
 
@@ -10,15 +14,43 @@ public partial class MainViewModel : ViewModelBase
     private string _greeting = "Welcome to Avalonia!";
 
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsImageSelected))]
     private string? _selectedFile;
-    
+
+    [ObservableProperty]
+    private Bitmap? _previewImage;
+
+    public bool IsImageSelected => SelectedFile is not null &&
+                                   new[] { ".png", ".jpg", ".jpeg", ".bmp" }
+                                       .Contains(Path.GetExtension(SelectedFile).ToLower());
+
     public ObservableCollection<string> Files { get; } = new();
 
+    partial void OnSelectedFileChanged(string? value)
+    {
+        PreviewImage?.Dispose();
+        PreviewImage = null;
+
+        if (value is not null && IsImageSelected)
+        {
+            try
+            {
+                PreviewImage = new Bitmap(value);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to load image: {e.Message}");
+            }
+        }
+    }
+    
     [RelayCommand]
     private void AddFiles()
     {
-        Files.Add($"C:/Test/image_{Files.Count + 1}.jpg");
-        Files.Add($"C:/Test/video_{Files.Count + 1}.mp4");
+        var testImagePath = @"C:\Users\MinecAnton209\Pictures\Screenshots\Screenshot 2025-10-29 135548.png";
+        
+        Files.Add(testImagePath);
+        SelectedFile = Files.LastOrDefault();
     }
 
     [RelayCommand]
